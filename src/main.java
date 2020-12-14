b701816ce80f595282c0b8b1551f4d556d721730
qwe <ee>
2020-12-14 18:55:16 +0300

    init queue

--- src/main.java
+++ src/main.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args){
-        HS.getHS();
-        LHS.getLHS();
-        ES.getES();
-        SS.getSS();
-        TS.getTS();
     }
 }
 
