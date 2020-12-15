9358fed5ac607f647e858a7bb1a7de7b33bc26ce
qwe <ee>
2020-12-15 17:58:40 +0300

    added simple io doings

--- src/main.java
+++ src/main.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args){
-
+        IOExample.readFile();
+        StreamTokenizerExample.getExample();
+//        URLExample.getExample(); //troubles
     }
 }
 
