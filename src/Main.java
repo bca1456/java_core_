b928142ddaa7acf45786ae4df1b8e8c81a4f6333
qwe <ee>
2020-12-30 15:26:15 +0300

    example of functional programming

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,51 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.util.function.Function;
 
-@SuppressWarnings("ALL")
 public class Main {
     public static void main(String[] args) {
-        final Collection<String> stringCollection= new HashSet<>();
-        stringCollection.add("1");
-        stringCollection.add("2");
-        stringCollection.add("3");
-        Iterator i = stringCollection.iterator();
-        i.remove();
-        System.out.println(stringCollection);
+        Collection<Integer> coll = new ArrayList<>();
+        coll.add(1);
+        coll.add(0);
+        coll.add(-3);
+        coll.add(5);
+        System.out.println(joinNumbers(coll));
     }
 
+
+
+    // Берет поэлементно значения из коллекции from, преобразует их с помощью функции transformer
+// и возвращает список результатов преобразования в том же порядке.
+    public static <F, T> List<T> map(Collection<F> from, Function<? super F, ? extends T> transformer) {
+        ArrayList<T> result = new ArrayList<T>();
+        for (F element : from)
+            result.add(transformer.apply(element));
+        return result;
+    }
+
+    // Берет коллекцию произвольных элементов и конкатенирует их в строку
+    public static <T> String join(Collection<T> from, String separator) {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (T element : from) {
+            if (first)
+                first = false;
+            else
+                result.append(separator);
+            result.append(element);
+        }
+        return result.toString();
+    }
+
+
+    public static String joinNumbers(Collection<? extends Integer> numbers) {
+        // Объявляем "функцию", присваиваем ее переменной intToString.
+        Function<Integer, String> intToString = new Function<Integer, String>() {
+            @Override public String apply(Integer from) {
+                return from.toString();
+            }
+        };
+        return join(map(numbers, intToString), ", ");
+    }
 }
 
 
